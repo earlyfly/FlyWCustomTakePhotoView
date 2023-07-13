fa91263f8b650e98a863894477a79094548d0bfb
Fly.W <wangfei1@trs.com.cn>
2023-07-13 16:58:00 +0800

    优化拍照后确认逻辑及UI交互，支持相机从相册选择

--- PhotoDemo/PhotoDemo/FlyW/GZIDCardTakePhotoVC.m
+++ PhotoDemo/PhotoDemo/FlyW/GZIDCardTakePhotoVC.m
@@ -8,13 +8,14 @@
 
 #import "GZIDCardTakePhotoVC.h"
 #import <Photos/Photos.h>
-#import <Masonry/Masonry.h>
+#import "Masonry.h"
+#import "TZImagePickerController.h"
 #import "UIImage+info.h"
 
 #define ScreenWidth [UIScreen mainScreen].bounds.size.width
 #define ScreenHeight [UIScreen mainScreen].bounds.size.height
 
-@interface GZIDCardTakePhotoVC ()<AVCaptureMetadataOutputObjectsDelegate,AVCapturePhotoCaptureDelegate,UIGestureRecognizerDelegate>
+@interface GZIDCardTakePhotoVC ()<AVCaptureMetadataOutputObjectsDelegate, AVCapturePhotoCaptureDelegate, UIGestureRecognizerDelegate, TZImagePickerControllerDelegate>
 
 
 //session：由他把输入输出结合在一起，并开始启动捕获设备（摄像头）
@@ -52,10 +53,10 @@
 //是否可以拍照
 @property (nonatomic, assign) BOOL canCa;
 //照片目标区域边框
-@property (nonatomic, strong) UIImageView *kuangImgView;
+@property (nonatomic, strong) UIImageView *clipAreaImgView;
 @property (nonatomic, strong) UIImage *areaImage;
 //提示文案
-@property (nonatomic, strong) UILabel *tishiLabel;
+@property (nonatomic, strong) UILabel *tipsLabel;
 
 @end
 
@@ -82,125 +83,79 @@
     }
 }
 
-#pragma mark - 检查相机权限
-- (BOOL)canUserCamear{
-    AVAuthorizationStatus authStatus = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
-    if (authStatus == AVAuthorizationStatusDenied) {
-        
-        UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:@"请打开相机权限" message:@"请前往系统设置-隐私-相机打开权限" preferredStyle:UIAlertControllerStyleAlert];
-        
-        UIAlertAction *sureAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-            NSURL * url = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
-            
-            if([[UIApplication sharedApplication] canOpenURL:url]) {
-                
-                [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
-                
-            }
-        }];
-        [alertVC addAction:sureAction];
-        
-        UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil];
-        [alertVC addAction:cancelAction];
-        
-        [self presentViewController:alertVC animated:YES completion:nil];
-        
-        return NO;
-    }
-    else{
-        return YES;
-    }
-    return YES;
+
+#pragma mark - Events
+// 取消 返回上级
+-(void)cancleHandler {
+    [self.imageView removeFromSuperview];
+    [self.session stopRunning];
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
-#pragma mark - 自定义视图
-- (void)setupView {
+// 重新拍照
+- (void)retakeNewPhoto {
+    self.imageView.image = nil;
+    [self.imageView removeFromSuperview];
     
-    UIEdgeInsets insets = [UIApplication sharedApplication].delegate.window.safeAreaInsets;
-    CGFloat top = MAX(insets.top, 20);
-    CGFloat bottom = insets.bottom;
-    
-    // 顶部操作区
-    [self.view addSubview:self.topView];
-    [self.topView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.right.equalTo(self.view);
-        make.top.equalTo(self.view);
-        make.height.mas_equalTo(50 + top);
-    }];
-    
-    // 底部操作区
-    [self.view addSubview:self.bottomView];
-    [self.bottomView addSubview:self.PhotoButton];
-    [self.bottomView addSubview:self.cancleButton];
-    [self.bottomView addSubview:self.selectPhotoBtn];
-    [self.bottomView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.right.equalTo(self.view);
-        make.bottom.equalTo(self.view);
-        make.height.mas_equalTo(80 + bottom);
-    }];
-    [self.PhotoButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerY.equalTo(self.bottomView).offset(-bottom/2);
-        make.centerX.equalTo(self.bottomView);
-    }];
-    
-    
-    [self.cancleButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerX.equalTo(self.bottomView).multipliedBy(0.5);
-        make.centerY.equalTo(self.PhotoButton);
-    }];
-    
-    [self.selectPhotoBtn mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerX.equalTo(self.bottomView).multipliedBy(1.5);
-        make.centerY.equalTo(self.PhotoButton);
-    }];
-    
-    [self.view addSubview:self.kuangImgView];
-    [self.kuangImgView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.size.mas_equalTo(self.areaImage.size);
-        make.center.equalTo(self.view);
-    }];
-    
-    [self.view addSubview:self.tishiLabel];
-    [self.tishiLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerY.equalTo(self.kuangImgView);
-        make.centerX.equalTo(self.kuangImgView.mas_left).offset(-20);
-    }];
-    
-    // 添加聚焦手势
-    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(focusGesture:)];
-    tapGesture.delegate = self;
-    [self.view addGestureRecognizer:tapGesture];
-    
+    [self configViewIsShouldTakePhoto:YES];
 }
-#pragma mark - 自定义相机
-- (void)customCamera{
+
+// 相册选择
+- (void)selectPhotoHandler {
     
-    //生成会话，用来结合输入输出
-    self.session = [[AVCaptureSession alloc]init];
-    if ([self.session canSetSessionPreset:AVCaptureSessionPresetPhoto]) {
-        self.session.sessionPreset = AVCaptureSessionPresetPhoto;
+    TZImagePickerController *imagePickerVC = [[TZImagePickerController alloc] initWithMaxImagesCount:1 delegate:self];
+    imagePickerVC.allowPickingVideo = NO;
+    // 是否支持裁剪
+    imagePickerVC.allowCrop = YES;
+    // 设置裁剪区域
+    CGFloat width = ScreenWidth;
+    // 注意：裁剪区域的宽高和相册选择的裁剪区域宽高是相反的
+    CGFloat height = self.areaImage.size.width * width / self.areaImage.size.height;
+    CGFloat y = (ScreenHeight - height)/2.0;
+    imagePickerVC.cropRect = CGRectMake(0, y, width, height);
+    
+    imagePickerVC.navigationBar.translucent = NO;
+    imagePickerVC.modalPresentationStyle = UIModalPresentationFullScreen;
+    imagePickerVC.naviBgColor = [UIColor whiteColor];
+    imagePickerVC.iconThemeColor = [UIColor blueColor];
+    imagePickerVC.oKButtonTitleColorNormal = [UIColor blueColor];
+    imagePickerVC.oKButtonTitleColorDisabled = [UIColor lightGrayColor];
+    __weak typeof(self) weakSelf = self;
+    [self presentViewController:imagePickerVC animated:YES completion:^{
+        [weakSelf configViewIsShouldTakePhoto:NO];
+    }];
+}
+
+// 拍照
+- (void)shutterCamera {
+    
+    AVCaptureConnection * videoConnection = [self.photoOutPut connectionWithMediaType:AVMediaTypeVideo];
+    if (!videoConnection) {
+        NSLog(@"take photo failed!");
+        return;
     }
-    if ([self.session canAddInput:self.input]) {
-        [self.session addInput:self.input];
+    
+//    NSDictionary *outputSettings = [[NSDictionary alloc] init];
+//    AVCapturePhotoSettings* setting = [AVCapturePhotoSettings
+//    photoSettingsWithFormat:outputSettings];
+//    // 设置闪光灯打开。注意，执行这句代码时闪光灯并不会打开，而是进行拍照时会自动打开，闪烁，然后关闭
+//    setting.flashMode = AVCaptureFlashModeAuto;
+//    // 拍照，照片在代理方法里获取
+//    [self.ImageOutPut capturePhotoWithSettings:setting delegate:self];
+    
+    AVCapturePhotoSettings *set = [AVCapturePhotoSettings photoSettings];
+    [self.photoOutPut capturePhotoWithSettings:set delegate:self];
+
+}
+
+// 选择照片 返回上级
+- (void)selectImage {
+
+    // 回传目标图片，并返回
+    if (self.photoCallback) {
+        self.photoCallback(self.image);
     }
-    
-    if ([self.session canAddOutput:self.photoOutPut]) {
-        [self.session addOutput:self.photoOutPut];
-    }
-    
-    [self.view.layer addSublayer:self.previewLayer];
-    
-    //开始启动
-    [self.session startRunning];
-    if ([self.device lockForConfiguration:nil]) {
-        //自动白平衡
-        if ([self.device isWhiteBalanceModeSupported:AVCaptureWhiteBalanceModeAutoWhiteBalance]) {
-            [self.device setWhiteBalanceMode:AVCaptureWhiteBalanceModeAutoWhiteBalance];
-        }
-        
-        [self.device unlockForConfiguration];
-    }
-    
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 #pragma mark - UIGestureRecognizerDelegate
@@ -260,12 +215,10 @@
 - (void)captureOutput:(AVCapturePhotoOutput *)captureOutput didFinishProcessingPhoto:(nonnull AVCapturePhoto *)photo error:(nullable NSError *)error {
     
     NSData *data = [photo fileDataRepresentation];
-    self.image = [UIImage imageWithData:data];
-    
+    [self scaleAndClipImage:[UIImage imageWithData:data]];
 
-    [self.session stopRunning];
+    self.imageView.image = self.image;
     [self.view insertSubview:self.imageView belowSubview:self.topView];
-    NSLog(@"image size = %@",NSStringFromCGSize(self.image.size));
     [self configViewIsShouldTakePhoto:NO];
     
     // 保存图片到相册
@@ -288,62 +241,148 @@
 //    }
 //}
 
-#pragma mark - Events
-// 取消 返回上级
--(void)cancleHandler {
+#pragma mark - TZImagePickerControllerDelegate
+- (void)imagePickerController:(TZImagePickerController *)picker didFinishPickingPhotos:(NSArray<UIImage *> *)photos sourceAssets:(NSArray *)assets isSelectOriginalPhoto:(BOOL)isSelectOriginalPhoto infos:(NSArray<NSDictionary *> *)infos {
+    
     [self.imageView removeFromSuperview];
-    [self.session stopRunning];
-    [self dismissViewControllerAnimated:YES completion:nil];
+    
+    self.image = photos.firstObject;
+    self.imageView.image = self.image;
+    [self.view insertSubview:self.imageView belowSubview:self.topView];
 }
 
-// 重新拍照
-- (void)retakeNewPhoto {
-    self.imageView.image = nil;
-    [self.imageView removeFromSuperview];
-    [self.session startRunning];
+// 取消选择的回调
+- (void)tz_imagePickerControllerDidCancel:(TZImagePickerController *)picker {
     [self configViewIsShouldTakePhoto:YES];
 }
 
-- (void)configViewIsShouldTakePhoto:(BOOL)shouldTake {
-    self.PhotoButton.hidden = !shouldTake;
-    self.cancleButton.hidden = !shouldTake;
-    self.selectPhotoBtn.hidden = !shouldTake;
-    self.reCamButton.hidden = shouldTake;
-    self.sureButton.hidden = shouldTake;
+#pragma mark - Private
+// 检查相机权限
+- (BOOL)canUserCamear{
+    AVAuthorizationStatus authStatus = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+    if (authStatus == AVAuthorizationStatusDenied) {
+        
+        UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:@"请打开相机权限" message:@"请前往系统设置-隐私-相机打开权限" preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction *sureAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            NSURL * url = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
+            
+            if([[UIApplication sharedApplication] canOpenURL:url]) {
+                
+                [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
+                
+            }
+        }];
+        [alertVC addAction:sureAction];
+        
+        UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil];
+        [alertVC addAction:cancelAction];
+        
+        [self presentViewController:alertVC animated:YES completion:nil];
+        
+        return NO;
+    }
+    else{
+        return YES;
+    }
+    return YES;
 }
 
-- (void)selectPhotoHandler {
+// 自定义视图
+- (void)setupView {
+    
+    UIEdgeInsets insets = [UIApplication sharedApplication].delegate.window.safeAreaInsets;
+    CGFloat top = MAX(insets.top, 20);
+    CGFloat bottom = insets.bottom;
+    
+    // 顶部操作区
+    [self.view addSubview:self.topView];
+    [self.topView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.equalTo(self.view);
+        make.top.equalTo(self.view);
+        make.height.mas_equalTo(50 + top);
+    }];
+    
+    // 底部操作区
+    [self.view addSubview:self.bottomView];
+    [self.bottomView addSubview:self.PhotoButton];
+    [self.bottomView addSubview:self.cancleButton];
+    [self.bottomView addSubview:self.selectPhotoBtn];
+    [self.bottomView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.equalTo(self.view);
+        make.bottom.equalTo(self.view);
+        make.height.mas_equalTo(80 + bottom);
+    }];
+    [self.PhotoButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerY.equalTo(self.bottomView).offset(-bottom/2);
+        make.centerX.equalTo(self.bottomView);
+    }];
+    
+    
+    [self.cancleButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.bottomView).multipliedBy(0.5);
+        make.centerY.equalTo(self.PhotoButton);
+    }];
+    
+    [self.selectPhotoBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.bottomView).multipliedBy(1.5);
+        make.centerY.equalTo(self.PhotoButton);
+    }];
+    
+    [self.view addSubview:self.clipAreaImgView];
+    [self.clipAreaImgView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.size.mas_equalTo(self.areaImage.size);
+        make.center.equalTo(self.view);
+    }];
+    
+    [self.view addSubview:self.tipsLabel];
+    [self.tipsLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerY.equalTo(self.clipAreaImgView);
+        make.centerX.equalTo(self.clipAreaImgView.mas_left).offset(-20);
+    }];
+    
+    // 添加聚焦手势
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(focusGesture:)];
+    tapGesture.delegate = self;
+    [self.view addGestureRecognizer:tapGesture];
     
 }
-
-// 拍照
-- (void)shutterCamera {
+// 自定义相机
+- (void)customCamera{
     
-    AVCaptureConnection * videoConnection = [self.photoOutPut connectionWithMediaType:AVMediaTypeVideo];
-    if (!videoConnection) {
-        NSLog(@"take photo failed!");
-        return;
+    //生成会话，用来结合输入输出
+    self.session = [[AVCaptureSession alloc]init];
+    if ([self.session canSetSessionPreset:AVCaptureSessionPresetPhoto]) {
+        self.session.sessionPreset = AVCaptureSessionPresetPhoto;
+    }
+    if ([self.session canAddInput:self.input]) {
+        [self.session addInput:self.input];
     }
     
-//    NSDictionary *outputSettings = [[NSDictionary alloc] init];
-//    AVCapturePhotoSettings* setting = [AVCapturePhotoSettings
-//    photoSettingsWithFormat:outputSettings];
-//    // 设置闪光灯打开。注意，执行这句代码时闪光灯并不会打开，而是进行拍照时会自动打开，闪烁，然后关闭
-//    setting.flashMode = AVCaptureFlashModeAuto;
-//    // 拍照，照片在代理方法里获取
-//    [self.ImageOutPut capturePhotoWithSettings:setting delegate:self];
+    if ([self.session canAddOutput:self.photoOutPut]) {
+        [self.session addOutput:self.photoOutPut];
+    }
     
-    AVCapturePhotoSettings *set = [AVCapturePhotoSettings photoSettings];
-    [self.photoOutPut capturePhotoWithSettings:set delegate:self];
-
+    [self.view.layer addSublayer:self.previewLayer];
+    
+    //开始启动
+    [self.session startRunning];
+    if ([self.device lockForConfiguration:nil]) {
+        //自动白平衡
+        if ([self.device isWhiteBalanceModeSupported:AVCaptureWhiteBalanceModeAutoWhiteBalance]) {
+            [self.device setWhiteBalanceMode:AVCaptureWhiteBalanceModeAutoWhiteBalance];
+        }
+        
+        [self.device unlockForConfiguration];
+    }
+    
 }
 
-// 选择照片 返回上级
-- (void)selectImage {
-    
+// 缩放及裁减相机拍得的照片
+- (void)scaleAndClipImage:(UIImage *)image {
     // 相机实际区域的尺寸
     CGFloat width = ScreenWidth;
-    CGFloat height = self.image.size.height * width / self.image.size.width;
+    CGFloat height = image.size.height * width / image.size.width;
     
     // 缩放到屏幕宽对应尺寸的倍数
     CGFloat screenScale = 2.5;
@@ -357,7 +396,7 @@
     CGFloat areaH = self.areaImage.size.height * screenScale;
 
     // 缩放图片
-    UIImage *image = [UIImage image:self.image scaleToSize:CGSizeMake(targetW, targetH)];
+    image = [UIImage image:image scaleToSize:CGSizeMake(targetW, targetH)];
     
     // 裁减指定区域
     CGFloat x = (targetW - areaW) / 2;
@@ -371,12 +410,23 @@
                               orientation:UIImageOrientationLeft];
         
     self.image = image;
+}
 
-    // 回传目标图片，并返回
-    if (self.photoCallback) {
-        self.photoCallback(self.image);
+- (void)configViewIsShouldTakePhoto:(BOOL)shouldTake {
+    self.PhotoButton.hidden = !shouldTake;
+    self.cancleButton.hidden = !shouldTake;
+    self.selectPhotoBtn.hidden = !shouldTake;
+    self.clipAreaImgView.hidden = !shouldTake;
+    self.previewLayer.hidden = !shouldTake;
+    self.tipsLabel.hidden = !shouldTake;
+    self.reCamButton.hidden = shouldTake;
+    self.sureButton.hidden = shouldTake;
+    
+    if (shouldTake) {
+        [self.session startRunning];
+    } else {
+        [self.session stopRunning];
     }
-    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 #pragma mark - Setter & Getter
@@ -483,11 +533,11 @@
 }
 
 // 采集及最终裁减区域框
-- (UIImageView *)kuangImgView {
-    if (_kuangImgView == nil) {
-        _kuangImgView = [[UIImageView alloc] initWithImage:self.areaImage];
+- (UIImageView *)clipAreaImgView {
+    if (_clipAreaImgView == nil) {
+        _clipAreaImgView = [[UIImageView alloc] initWithImage:self.areaImage];
     }
-    return _kuangImgView;
+    return _clipAreaImgView;
 }
 
 - (UIImage *)areaImage {
@@ -498,20 +548,20 @@
 }
 
 // 提示文案
-- (UILabel *)tishiLabel {
+- (UILabel *)tipsLabel {
     
-    if (_tishiLabel == nil) {
-        _tishiLabel = [[UILabel alloc] init];
-        _tishiLabel.text = self.idCardType == GZIDCardTakeTypeBack ? @"请拍摄身份证国徽面，并尝试对其边缘" : @"请拍摄身份证人像面，并尝试对其边缘";
-        _tishiLabel.textColor = [UIColor whiteColor];
-        _tishiLabel.numberOfLines = 0;
-        _tishiLabel.textAlignment = NSTextAlignmentCenter;
-        _tishiLabel.font = [UIFont systemFontOfSize:15];
-        _tishiLabel.transform = CGAffineTransformMakeRotation(M_PI/2);
+    if (_tipsLabel == nil) {
+        _tipsLabel = [[UILabel alloc] init];
+        _tipsLabel.text = self.idCardType == GZIDCardTakeTypeBack ? @"请拍摄身份证国徽面，并尝试对其边缘" : @"请拍摄身份证人像面，并尝试对其边缘";
+        _tipsLabel.textColor = [UIColor whiteColor];
+        _tipsLabel.numberOfLines = 0;
+        _tipsLabel.textAlignment = NSTextAlignmentCenter;
+        _tipsLabel.font = [UIFont systemFontOfSize:15];
+        _tipsLabel.transform = CGAffineTransformMakeRotation(M_PI/2);
         
     }
     
-    return _tishiLabel;
+    return _tipsLabel;
 }
 
 // 使用self.session，初始化预览层，self.session负责驱动input进行信息的采集，layer负责把图像渲染显示
